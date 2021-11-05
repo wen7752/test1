8fb4899d2b31d5270729edbae40ce00f7eb1ea33
wen7752 <44072652+wen7752@users.noreply.github.com>
2021-11-05 09:53:52 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int a = 1;
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    std::cout << "Hello World!";
+    a ++;
+
+
+    cout << endl;
+
+    cout << endl;
+    a = a*a+a;
+    cout << endl;
+    return 0;
+}
